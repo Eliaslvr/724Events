623d5d0fb99609df2c2f8abd0c5f1be959cec1e2
Eliaslvr <eliaslvr59@gmail.com>
2024-06-24 11:27:07 +0200

    debug

---
+++ src/pages/Home/index.test.js
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./index";
+
+describe("When Form is created", () => {
+  it("a list of fields card is displayed", async () => {
+    render(<Home />);
+    // findByText cherche de manière asynchrone un élément avec le texte spécifié, ce qui permet d'attendre que l'élément soit ajouté au DOM. Renvoie une promesse.
+    await screen.findByText("Email");
+    await screen.findByText("Nom");
+    await screen.findByText("Prénom");
+    await screen.findByText("Personel / Entreprise");
+  });
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the success message is displayed", async () => {
+      render(<Home />);
+      // fireEvent est utilisé pour déclencher des événements
+      fireEvent(
+        await screen.findByText("Envoyer"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      await screen.findByText("En cours");
+      await screen.findByText("Message envoyé !");
+    });
+  });
+});
+
+
+describe("When a page is created", () => {
+  it("a list of events is displayed", () => {
+    // to implement
+  })
+  it("a list a people is displayed", () => {
+    // to implement
+  })
+  it("a footer is displayed", () => {
+    // to implement
+  })
+  it("an event card, with the last event, is displayed", () => {
+    // to implement
+  })
+});
